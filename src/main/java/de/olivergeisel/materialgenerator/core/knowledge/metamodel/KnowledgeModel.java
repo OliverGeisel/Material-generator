bc28991f6cdf9aa463e1a4be3878b1780b026afa
Oliver Geisel <geisel.oliver@googlemail.com>
2022-05-08 16:46:51 +0200

    Add Graph in KnowledgeModel

--- src/main/java/de/olivergeisel/materialgenerator/core/knowledge/metamodel/KnowledgeModel.java
+++ src/main/java/de/olivergeisel/materialgenerator/core/knowledge/metamodel/KnowledgeModel.java
@@ -3,6 +3,8 @@ package de.olivergeisel.materialgenerator.core.knowledge.metamodel;
 import de.olivergeisel.materialgenerator.core.knowledge.metamodel.element.KnowledgeElement;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultUndirectedGraph;
 
 import java.util.*;
 
@@ -11,10 +13,10 @@ public class KnowledgeModel {
 	private final Map<String, String> graph = new HashMap<>();
 	private final Map<String, Set<KnowledgeElement>> unknownRelations = new HashMap<>();
 
-	private Graph<KnowledgeElement, String> test;
+	private Graph<KnowledgeElement, DefaultEdge> test;
 
 	public KnowledgeModel() {
-		this.test = new DefaultDirectedGraph<>(String.class);
+		this.test = new DefaultUndirectedGraph<>(DefaultEdge.class);
 	}
 	public List<String> getIDs(){
 		return test.vertexSet().stream().map(KnowledgeElement::getId).toList();
@@ -39,7 +41,12 @@ public class KnowledgeModel {
 		return result;
 	}
 
+	private Collection<String> findMatchingIDs(String element){
+		return getIDs().stream().filter(it->it.split("-")[0].equals(element)).toList();
+	}
 	public void findAll(String element) {
-		var matchingIDs = getIDs().stream().filter(it->it.split("-")[0].equals(element));
+		var matchingIDs = findMatchingIDs(element);
+
+
 	}
 }
