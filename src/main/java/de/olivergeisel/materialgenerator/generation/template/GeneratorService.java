2076f00b65914ea0c036c11f24fcd812d87559b4
Oliver Geisel <geisel.oliver@googlemail.com>
2023-06-01 00:13:17 +0200

    Refactor GeneratorService and TemplateService
    Signed-off-by: Oliver Geisel <geisel.oliver@googlemail.com>

--- src/main/java/de/olivergeisel/materialgenerator/generation/template/GeneratorService.java
+++ src/main/java/de/olivergeisel/materialgenerator/generation/template/GeneratorService.java
@@ -1,76 +1,24 @@
 package de.olivergeisel.materialgenerator.generation.template;
 
-import de.olivergeisel.materialgenerator.core.knowledge.metamodel.KnowledgeModel;
-import de.olivergeisel.materialgenerator.core.knowledge.metamodel.element.KnowledgeElement;
-import org.apache.tomcat.util.json.JSONParser;
-import org.apache.tomcat.util.json.ParseException;
+import de.olivergeisel.materialgenerator.core.knowledge.KnowledgeManagement;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class GeneratorService {
-	private static final String KNOWLEDGE_PATH = "src/main/resources/data/knowledge/knowledgedata.json";
-	private static KnowledgeModel knowledge;
 
-	private static Object parseFromInputStream(InputStream input) {
-		var parser = new JSONParser(input);
-		Object parsedObject;
-		try {
-			parsedObject = parser.parse();
-		} catch (ParseException e) {
-			throw new RuntimeException(e);
-		}
-		return parsedObject;
-	}
+	private final KnowledgeManagement knowledgeManagement;
 
-	private static Object parseFile(File file) {
-		InputStream input;
-		try {
-			input = new FileInputStream(file);
-			return parseFromInputStream(input);
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private static Object pareseFromPath(String path) {
-		File file = new File(path);
-		return parseFile(file);
-	}
-
-	private void createKnowledgeModel(Object parsed) {
-		knowledge = new KnowledgeModel();
-		var model = (List<Map<String, ?>>) parsed;
-
-		for (var element : model) {
-			var jsonElement = (Map<String, ?>) element;
-			var correct = getKnowledgeElement(jsonElement);
-			knowledge.add(correct);
-		}
-	}
-
-	private KnowledgeElement getKnowledgeElement(Map<String, ?> jsonElement) {
-		// TODO: 2023-5-21 fixing this
-		var type = jsonElement.get("typ").toString();
-		var content = jsonElement.get("content").toString();
-		var id = jsonElement.get("id").toString();
-		String structure = jsonElement.get("structure").toString();
-		var relationsStrings = (List<String>) jsonElement.get("relations");
-		//var relations = relationsStrings.stream().map(Relation::new).toList();
-		return null;
-		//ElementGenerator.create(type, id, content, relations);
+	public GeneratorService(KnowledgeManagement knowledgeManagement) {
+		this.knowledgeManagement = knowledgeManagement;
 	}
 
 	public Map<String, String> getPlain(String definition) {
 		Map<String, String> back = new HashMap<>();
-		List<Map<String, ?>> objects = (List<Map<String, ?>>) pareseFromPath(KNOWLEDGE_PATH);
+		List<Map<String, ?>> objects = knowledgeManagement.getNewModelAsJSON();
 		for (var element : objects) {
 			if (element.get("typ").equals("Definition") && ((String) element.get("id")).contains(definition)) {
 				back.put("term", definition.split("-")[0]);
@@ -81,13 +29,10 @@ public class GeneratorService {
 		return back;
 	}
 
-	//
-//
-	public KnowledgeModel getKnowledge() {
-		if (knowledge == null) {
-			var parsed = pareseFromPath(KNOWLEDGE_PATH);
-			createKnowledgeModel(parsed);
-		}
-		return knowledge;
+//region getter / setter
+	public Map<String, String> getIllustrated() {
+		return null;
 	}
+//endregion
+
 }
--- src/main/java/de/olivergeisel/materialgenerator/generation/template/TemplateService.java
+++ src/main/java/de/olivergeisel/materialgenerator/generation/template/TemplateService.java
@@ -2,10 +2,62 @@ package de.olivergeisel.materialgenerator.generation.template;
 
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 @Service
 public class TemplateService {
 
-	public TemplateSet getTemplateSet(String id) {
+	public static final String TEMPLATE_SET_FROM_TEMPLATES_FOLDER = "../templateSets/";
+	private final String TEMPLATE_SET_PATH = "src/main/resources/templateSets/";
+
+	public TemplateSet createTemplateSet(String dir, TemplateSet templateSet) {
+		File newFolder = new File(TEMPLATE_SET_PATH + dir);
+		if (newFolder.exists()) {
+			throw new IllegalArgumentException("TemplateSet with name " + dir + " already exists");
+		}
+		newFolder.mkdir();
 		return null;
+	}
+
+	public boolean deleteTemplateSet(String id) {
+		File dir = new File(TEMPLATE_SET_PATH + id);
+		if (dir.exists()) {
+			try {
+				Files.delete(dir.toPath());
+			} catch (IOException e) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	public TemplateSet getTemplateSet(String name) {
+		File dir = new File(TEMPLATE_SET_PATH + name);
+		if (dir.exists()) {
+			return loadTemplateSet(dir);
+		}
+		return null;
+	}
+
+	private Template loadTemplate(File file) {
+		var typeString = file.getName().replace(".html", "").toUpperCase();
+		TemplateType type = TemplateType.valueOf(typeString);
+		Template template = new DefinitionTemplate(file);
+		return template;
+	}
+
+	private TemplateSet loadTemplateSet(File dir) {
+		TemplateSet templateSet = new TemplateSet();
+		templateSet.setName(dir.getName());
+		for (File file : dir.listFiles()) {
+			if (file.isDirectory()) {
+				templateSet.addAllTemplates(loadTemplateSet(file));
+			} else {
+				templateSet.addTemplate(loadTemplate(file));
+			}
+		}
+		return templateSet;
 	}
 }
