ea0baca1b44ed52690c4390a4bec666593e94f8d
Oliver Geisel <geisel.oliver@googlemail.com>
2023-06-01 02:58:03 +0200

    Add content to source
    Signed-off-by: Oliver Geisel <geisel.oliver@googlemail.com>

--- src/main/java/de/olivergeisel/materialgenerator/core/knowledge/metamodel/source/KnowledgeSource.java
+++ src/main/java/de/olivergeisel/materialgenerator/core/knowledge/metamodel/source/KnowledgeSource.java
@@ -3,9 +3,41 @@ package de.olivergeisel.materialgenerator.core.knowledge.metamodel.source;
 public abstract class KnowledgeSource {
 	private String id;
 	private String name;
+	private String content;
 
 	protected KnowledgeSource(String id, String name) {
 		this.id = id;
 		this.name = name;
+	}
+
+	//region getter / setter
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+//endregion
+
+	@Override
+	public int hashCode() {
+		return id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof KnowledgeSource that)) return false;
+
+		return id.equals(that.id);
 	}
 }
