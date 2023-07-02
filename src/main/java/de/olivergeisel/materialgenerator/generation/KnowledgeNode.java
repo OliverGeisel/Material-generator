884533bf9d817f0ef41c366d0c451ce7eec0a97e
Oliver Geisel <geisel.oliver@googlemail.com>
2023-07-03 01:40:54 +0200

    Extend KnowledgeNode with an optional goal
    Signed-off-by: Oliver Geisel <geisel.oliver@googlemail.com>

--- src/main/java/de/olivergeisel/materialgenerator/generation/KnowledgeNode.java
+++ src/main/java/de/olivergeisel/materialgenerator/generation/KnowledgeNode.java
@@ -1,18 +1,26 @@
 package de.olivergeisel.materialgenerator.generation;
 
+import de.olivergeisel.materialgenerator.core.courseplan.content.ContentGoal;
+import de.olivergeisel.materialgenerator.core.courseplan.content.ContentGoalExpression;
 import de.olivergeisel.materialgenerator.core.knowledge.metamodel.element.KnowledgeElement;
 import de.olivergeisel.materialgenerator.core.knowledge.metamodel.relation.Relation;
 import de.olivergeisel.materialgenerator.core.knowledge.metamodel.structure.KnowledgeObject;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
 
+/**
+ * Contains all Knowledge from a KnowledgeModel, that is related to a specific topic in the structure.
+ * Each Node has a main KnowledgeElement. It should be a Term
+ */
 public class KnowledgeNode {
 
 	private KnowledgeObject structurePoint;
 	private KnowledgeElement mainElement;
 	private KnowledgeElement[] relatedElements;
 	private Relation[] relations;
+	private Optional<ContentGoal> goal = Optional.empty();
 
 	public KnowledgeNode(KnowledgeObject structurePoint, KnowledgeElement mainElement, KnowledgeElement[] relatedElements, Relation[] relations) {
 		this.structurePoint = structurePoint;
@@ -22,6 +30,15 @@ public class KnowledgeNode {
 	}
 
 	//region setter/getter
+	public Optional<ContentGoalExpression> getExpression() {
+		return goal.map(ContentGoal::getExpression);
+	}
+
+	public Optional<String> getMasterKeyWord() {
+		return goal.map(ContentGoal::getMasterKeyword);
+	}
+
+
 	public KnowledgeObject getStructurePoint() {
 		return structurePoint;
 	}
@@ -53,6 +70,13 @@ public class KnowledgeNode {
 	public void setRelations(Relation[] relations) {
 		this.relations = relations;
 	}
+
+	public void setGoal(ContentGoal goal) {
+		if (goal == null) {
+			throw new IllegalArgumentException("goal must not be null");
+		}
+		this.goal = Optional.of(goal);
+	}
 //endregion
 
 	@Override
@@ -76,6 +100,5 @@ public class KnowledgeNode {
 		result = 31 * result + Arrays.hashCode(relations);
 		return result;
 	}
-//endregion
 
 }
